619f5d0ef6708f6c1427172fa508ba513b291492
Přemysl Strakoš <premysl.strakos@seznam.cz>
2019-09-19 12:43:44 +0200

    Few little changes

--- max31865.c
+++ max31865.c
@@ -2,8 +2,8 @@
  * max31865.c
  *
  * Created: 13.9.2019 12:34:48
- * Author : GeorgeShift
- */
+ * Author : strakos.p
+ */ 
 
 // Includes
 #include "max31865.h"
@@ -19,30 +19,30 @@
 
 // Reading data from SPI
 char max_spi_read(char addr){
-
+	
 	CS_PORT &= ~_BV(CS_PIN);	// CS_LOW - activate slave
-
+	
 	SPDR = addr;				// register address to read
 	while(!(SPSR & (1<<SPIF)));	// wait for complete sending
 
 	SPDR = 0xFF;				// dummy data to provide SPI clock signals to read
 	while(!(SPSR & (1<<SPIF)));	// wait for complete sending
-
+	
 	CS_PORT |= _BV(CS_PIN);		// CS_HIGH - deactivate slave
 	return SPDR;				// delete flag SPIF and return data
 }
 
 // Writing data to SPI
 void max_spi_write(char addr, char data){
-
+	
 	CS_PORT &= ~_BV(CS_PIN);	// CS_LOW - activate slave
-
+	
 	SPDR = addr;				// register address to write
 	while(!(SPSR & (1<<SPIF)));	// wait for complete sending
-
+	
 	SPDR = data;				// data to write
 	while(!(SPSR & (1<<SPIF)));	// wait for complete sending
-
+	
 	CS_PORT |= _BV(CS_PIN);		// CS_HIGH - deactivate slave
 }
 
@@ -65,10 +65,12 @@ void max_init_port(void)
 	DDR(CS_PORT) |= _BV(CS_PIN);
 	DDR(MOSI_PORT) |= _BV(MOSI_PIN);
 	DDR(SCK_PORT) |= _BV(SCK_PIN);
-
+	
+	DDRB |= (1 << DDB2);	// SS
+	
 	// set outputs
 	CS_PORT |= _BV(CS_PIN);	// CS HIGH, slave disabled
-
+	
 	// input DDRs
 	DDR(DRDY_PORT) &= ~_BV(DRDY_PIN);
 	DDR(MISO_PORT) &= ~_BV(MISO_PIN);
@@ -80,19 +82,19 @@ uint8_t init_max(void)
 	/*
 		If communication is done properly function returns 1, otherwise returns 0
 	*/
-
+	
 	uint8_t conf = 0;
-
+	
 	max_spi_write(CONFIGURATION, 0b10000000);	// Enable V bias
 	conf = max_spi_read(READ_CONFIGURATION);	// Reading Configuration register to verify communication
-
+	
 	if(conf == 0b10000000)
 	{
 		max_spi_write(WRITE_HIGH_FAULT_TRESHOLD_MSB, 0xFF);	// Writing High Fault Threshold MSB
 		max_spi_write(WRITE_HIGH_FAULT_TRESHOLD_LSB, 0xFF);	// Writing High Fault Threshold LSB
 		max_spi_write(WRITE_LOW_FAULT_TRESHOLD_MSB, 0x00);		// Writing Low Fault Threshold MSB
 		max_spi_write(WRITE_LOW_FAULT_TRESHOLD_LSB, 0x00);		// Writing Low Fault Threshold LSB
-
+		
 		return 1;
 	}
 	else
@@ -101,46 +103,54 @@ uint8_t init_max(void)
 	}
 }
 
+// Fault test
+uint8_t max_fault_test(void)
+{
+	/*
+		Returns 0 if there is not fault detected
+	*/
+	
+	uint8_t  lsb_rtd;
+	
+	lsb_rtd = max_spi_read(READ_RTD_LSB);
+	
+	return lsb_rtd&0x01;	
+}
+
 // Reading data from max
-float max_get_data(char datatype)
+long max_get_data(char datatype)
 {
 	/*
 		Parameter datatype is for choose between temperature and RTD as return value, type:
 		'r' for RTD
 		't' for temperature.
-
+		
 		In normal operation function returns RTD or temperature multiplexed by 10
-		If new conversion result is not available - DRDY is high, returns 0
-		If any error in fault register have been detected returns 1
-		In the case of invalid parameter datatype returns 2
+		If new conversion result is not available - DRDY is high, returns 1000
 	*/
-
+	
 	uint8_t lsb_rtd, msb_rtd;
-	uint8_t fault_test;
 	uint8_t DRDY_state;
-
+	
 	int RTD;
 	long temperature;
 
 	DRDY_state = DRDY_PORT&(1 << DRDY_PIN);
-	lsb_rtd = max_spi_read(READ_RTD_LSB);
-	fault_test = lsb_rtd&0x01;
 
-
-	if (fault_test == 0)
+	if (DRDY_state == 0)
 	{
-		if (DRDY_state == 0)
-		{
-			msb_rtd = max_spi_read(READ_RTD_MSB);
-
-			RTD = ((msb_rtd << 7)+((lsb_rtd & 0xFE) >> 1));
-			if (datatype == 'r') return RTD;
-
-			temperature = ( (long)10*RTD >> 5) - 2560;	// RTD / 32 - 256
-			if (datatype == 't') return temperature;
-			else return 2;
-		}
-		else return 0;
+		lsb_rtd = max_spi_read(READ_RTD_LSB);
+		msb_rtd = max_spi_read(READ_RTD_MSB);
+			
+		RTD = ((msb_rtd << 7)+((lsb_rtd & 0xFE) >> 1));
+		if (datatype == 'r') return RTD;
+			
+		// Basic temperature calculation, the accuracy for temperatures 
+		// between -75°C - 100°C is max +-1.5°C
+		temperature = ( (long)10*RTD >> 5) - 2560;	// RTD / 32 - 256
+		if (datatype == 't') return temperature;
 	}
-	else return 1;
+	// return 1000 if not conversion available
+	else return 1000;
 }
+
--- max31865.h
+++ max31865.h
@@ -2,8 +2,8 @@
  * max31865.h
  *
  * Created: 13.9.2019 12:37:00
- * Author: GeorgeShift
- */
+ * Author: strakos.p
+ */ 
 
 #ifndef MAX31865_H_
 #define MAX31865_H_
@@ -11,11 +11,11 @@
 	// Define your CPU frequency
 	#define F_CPU 8000000
 	/* ********************************* */
-
+	
 	#include <avr/io.h>
 	#include <avr/interrupt.h>
 	#include <util/delay.h>
-
+	
 	// Define your SPI pins
 	/* ********************************* */
 	#define CS_PORT		PORTB
@@ -28,12 +28,12 @@
 	#define MISO_PIN	4
 	#define SCK_PORT	PORTB
 	#define SCK_PIN		5
-
+	
 	//Registers defined in Table 1 on page 12 of the datasheet
-	#define READ_CONFIGURATION						0b00000000	// 0x00H
-	#define CONFIGURATION									0b10000000	// 0x80H
-	#define READ_RTD_MSB									0b00000001	// 0x01H
-	#define READ_RTD_LSB									0b00000010	// 0x02H
+	#define READ_CONFIGURATION				0b00000000	// 0x00H
+	#define CONFIGURATION					0b10000000	// 0x80H
+	#define READ_RTD_MSB					0b00000001	// 0x01H
+	#define READ_RTD_LSB					0b00000010	// 0x02H
 	#define READ_HIGH_FAULT_TRESHOLD_MSB	0b00000011	// 0x03H
 	#define WRITE_HIGH_FAULT_TRESHOLD_MSB	0b10000011	// 0x83H
 	#define READ_HIGH_FAULT_TRESHOLD_LSB	0b00000100	// 0x04H
@@ -42,12 +42,13 @@
 	#define WRITE_LOW_FAULT_TRESHOLD_MSB	0b10000101	// 0x85H
 	#define READ_LOW_FAULT_TRESHOLD_LSB		0b00000110	// 0x06H
 	#define WRITE_LOW_FAULT_TRESHOLD_LSB	0b10000110	// 0x86H
-	#define FAULT_STATUS									0b00000111	// 0x07H
-
+	#define FAULT_STATUS					0b00000111	// 0x07H
+	
 	extern char max_spi_read(char addr);
 	extern void max_spi_write(char addr, char data);
 	extern void max_init_port(void);
 	extern uint8_t init_max(void);
-	extern float max_get_data(char datatype);
-
+	extern uint8_t max_fault_test(void);
+	extern long max_get_data(char datatype);
+		
 #endif /* MAX31865_H_ */
